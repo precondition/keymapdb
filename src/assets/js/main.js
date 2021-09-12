24c49fb9437569661553642926b57b6917c1a530
precondition <57645186+precondition@users.noreply.github.com>
2021-09-12 16:14:38 +0200

    Don't lazy load images
    I don't want JS to be required to see the pictures

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -34,18 +34,3 @@ menuToggle.addEventListener('click', () => {
   menu.classList.toggle('hidden');
   searchBox.classList.toggle('hidden');
 }, false);
-
-/*
-Lazy load images
-*/
-
-const lazyImages = document.getElementsByClassName('lazy');
-
-document.addEventListener('DOMContentLoaded', () => {
-  [...lazyImages].forEach((elem) => {
-    const originalImage = elem.dataset.src;
-
-    elem.setAttribute('src', originalImage);
-    elem.removeAttribute('data-src');
-  });
-}, false);
