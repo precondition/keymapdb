454a9231cc67902315acbee9dd5e0bd3e7631673
precondition <57645186+precondition@users.noreply.github.com>
2022-02-25 01:53:34 +0100

    Rewrite search.js in a simpler non-OOP way

--- src/assets/js/search.js
+++ src/assets/js/search.js
@@ -1,50 +1,42 @@
-class SearchPosts {
-  async init(indexJSONpath) {
-    const params = new URL(location.href).searchParams;
-    this.posts = await fetch(indexJSONpath).then((res) => {
-      return res.json();
-    });
-    this.render(params);
-  }
-
-  render(query) {
-    const matchedPosts = this.posts.filter((post) => {
-      for (const [queryKey, value] of query) {
-        if (queryKey.endsWith("Count")) {
-          let lowerbound, upperbound;
-          [lowerbound, upperbound] = value.split("-").map(Number);
-          if (upperbound === undefined && !isNaN(lowerbound)) {
-            // `value` does not contain a "-" and is thus a
-            // single number as opposed to a range.
-            upperbound = lowerbound;
-          }
-          const postCount = Number(post[queryKey]);
-          if (!(lowerbound <= postCount && postCount <= upperbound)) {
-            return false;
-          }
-        } else if (post[queryKey] instanceof Array) {
-            const selectedValues = value.split(",");
-            if (!selectedValues.some(x => new Set(post[queryKey]).has(x))) {
-                return false;
-            }
-        } else if (post[queryKey] !== value) {
-          console.log("Returning false because " + post[queryKey] + " != " + value);
-          return false;
-        }
+function isKeymapConforming(query, keymapData) {
+  for (const [queryKey, value] of query) {
+    if (queryKey.endsWith("Count")) {
+      let lowerbound, upperbound;
+      [lowerbound, upperbound] = value.split("-").map(Number);
+      if (upperbound === undefined && !isNaN(lowerbound)) {
+        // `value` does not contain a "-" and is thus a
+        // single number as opposed to a range.
+        upperbound = lowerbound;
       }
-      return true;
-    });
-    console.log("matchedPosts is \n");
-    console.log(matchedPosts);
+      const countValue = Number(keymapData[queryKey]);
+      if (!(lowerbound <= countValue && countValue <= upperbound)) {
+        return false;
+      }
+    } else if (keymapData[queryKey] instanceof Array) {
+        const selectedValues = value.split(",");
+        if (!selectedValues.some(x => new Set(keymapData[queryKey]).has(x))) {
+            return false;
+        }
+    // In case the keymap data value is a boolean, `value` must be deserialized into a boolean, as done after the &&
+    } else if (keymapData[queryKey] !== value && keymapData[queryKey] !== (value === "true")) {
+      console.log("Returning false because " + keymapData[queryKey] + " != " + value);
+      return false;
+    }
   }
+  return true;
 }
 
+async function getFilteredKeymaps() {
+      const keymapsJSON = await fetch("{{'/index.json' | url}}").then(res => res.json());
+      const searchParams = new URLSearchParams(location.search);
+      return keymapsJSON.filter(keymap => isKeymapConforming(searchParams, keymap));
+}
 
 const pageRegExp = new RegExp("{{ '/page/' | url }}[0-9]+");
-if (location.pathname === "{{'/' | url }}") {
-  const searchPosts = new SearchPosts();
-  searchPosts.init("index.json");
-} else if (pageRegExp.test(location.pathname)) {
-  const searchPosts = new SearchPosts();
-  searchPosts.init("../../index.json");
+if (location.pathname === "{{'/' | url }}" || pageRegExp.test(location.pathname)) {
+    (async() => {
+      let filteredKeymaps = await getFilteredKeymaps();
+      console.log("filtered keymaps ↓");
+      console.log(filteredKeymaps);
+    })();
 }
