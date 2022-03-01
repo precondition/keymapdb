44021cb4cdf54687301614ce7d09802a2d154301
precondition <57645186+precondition@users.noreply.github.com>
2022-03-01 13:30:49 +0100

    Increase pagination to 18 and update desc

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,11 +1,11 @@
 module.exports = {
   title: 'KeymapDB',
-  description: 'KeymapDB as a filterable pseudo-blog, proof-of-concept',
-  keywords: ['eleventy', 'template', 'simple', 'clean'],
+  description: 'Online database for keymaps of programmable keyboards',
+  keywords: ["QMK", "keyboard", "keymap", "remapping", "database", "static"],
   // your site url without trailing slash
-  url: 'https://vredeburg.netlify.app',
+  //url: 'https://vredeburg.netlify.app',
   // how many posts you want to show for each page
-  paginate: 9
+  paginate: 18
   // if you want to add disqus to your site
   // disqusShortname: "your-shortname"
 };
