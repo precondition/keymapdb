d93e3aacd221f69af0536ccfd46d71bdfce6c7de
precondition <57645186+precondition@users.noreply.github.com>
2022-03-01 13:31:53 +0100

    More updates to _data/site

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: 'KeymapDB',
-  description: 'Online database for keymaps of programmable keyboards',
-  keywords: ["QMK", "keyboard", "keymap", "remapping", "database", "static"],
+  description: 'Public database for keymaps of programmable keyboards',
+  keywords: ["QMK", "keyboard", "keymap", "remapping", "database", "static", "open-source"],
   // your site url without trailing slash
   //url: 'https://vredeburg.netlify.app',
   // how many posts you want to show for each page
