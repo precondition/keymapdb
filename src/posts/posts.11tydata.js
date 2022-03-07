e26777c6cffdc4b4f883262ef1f4e8103bf1b42b
precondition <57645186+precondition@users.noreply.github.com>
2022-03-07 22:17:19 +0100

    Restore posts.11tydata.js
    Without it, the posts were permalinked to
    `/posts/keyboard_name-username` instead of just
    `/keyboard_name-username`.

---
+++ src/posts/posts.11tydata.js
@@ -0,0 +1,15 @@
+module.exports = {
+  eleventyComputed: {
+    permalink: (data) => `${data.page.fileSlug}/index.html`,
+    keymapImage: (data) => {
+      if (data.keymapImage) {
+        if (data.keymapImage.search(/^https?:\/\//) !== -1) {
+          return data.keymapImage;
+        }
+        return `/assets/img/${data.keymapImage}`;
+      } else {
+        return false;
+      }
+    }
+  }
+};
