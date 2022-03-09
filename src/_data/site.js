491ecb7323b27ba707665eeb8b205f467c06c7dd
precondition <57645186+precondition@users.noreply.github.com>
2022-03-10 00:44:21 +0100

    Update _data/site.js

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: 'KeymapDB',
-  description: 'Public database for keymaps of programmable keyboards',
-  keywords: ["QMK", "keyboard", "keymap", "remapping", "database", "static", "open-source"],
+  description: 'Database of keymaps for programmable keyboards',
+  keywords: ["QMK", "keyboard", "keymap", "remapping", "database", "open-source", "mechanical keyboards", "ergomech", "keycap", "custom", "customization", "ZMK", "typing", "touchtyping", "macros", "inspiration", "ideas", "QWERTY", "Colemak", "Dvorak", "keyboard layout"],
   // your site url without trailing slash
   //url: 'https://vredeburg.netlify.app',
   // how many posts you want to show for each page
