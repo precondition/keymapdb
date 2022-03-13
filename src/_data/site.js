927627b22f19338a4c05387d6ea16549183dfea2
precondition <57645186+precondition@users.noreply.github.com>
2022-03-13 20:42:10 +0100

    Remove commented out code in site.js

--- src/_data/site.js
+++ src/_data/site.js
@@ -2,10 +2,6 @@ module.exports = {
   title: 'KeymapDB',
   description: 'Database of keymaps for programmable keyboards',
   keywords: ["QMK", "keyboard", "keymap", "remapping", "database", "open-source", "mechanical keyboards", "ergomech", "keycap", "custom", "customization", "ZMK", "typing", "touchtyping", "macros", "inspiration", "ideas", "QWERTY", "Colemak", "Dvorak", "keyboard layout"],
-  // your site url without trailing slash
-  //url: 'https://vredeburg.netlify.app',
   // how many posts you want to show for each page
   paginate: 18
-  // if you want to add disqus to your site
-  // disqusShortname: "your-shortname"
 };
