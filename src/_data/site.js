5dc16166dacbe3192a4208a98f989c0d004efa33
precondition <57645186+precondition@users.noreply.github.com>
2022-03-16 01:13:21 +0100

    Use short description in tab title

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,5 +1,6 @@
 module.exports = {
   title: 'KeymapDB',
+  shortDescription: "Database of keymaps for programmable keyboards",
   description: "KeymapDB is a public and open-source online database for keymaps of programmable keyboards, with a focus on QMK ergonomic mechanical keyboards. You can filter user-submitted keymaps like Miryoku by key count, firmware, keyboard, stagger, languages, OS, home row mods, and more!",
   keywords: ["QMK", "keyboard", "keymap", "remapping", "database", "open-source", "mechanical keyboards", "ergomech", "keycap", "custom", "customization", "ZMK", "typing", "touchtyping", "macros", "inspiration", "ideas", "QWERTY", "Colemak", "Dvorak", "keyboard layout"],
   // how many posts you want to show for each page
